2a2eb88c2d11972eeb54c1de69b2f0a5877147ff
preqsy <obbyprecious24@gmail.com>
2025-11-06 05:32:26 +0100

    refactor: Remove unused set -e and export commands from entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-set -e
-export RELEASE_VERSION=`sed -n 's/^ *version.*=.*"\([^"]*\)".*/\1/p' pyproject.toml`
 poetry run alembic upgrade head
 
-# Launch uvicorn and replace the caller as Process #1 in the container
-exec "$@"
+poetry run arq task_queue.main.WorkerSettings &
+
+poetry run uvicorn main:app --host 0.0.0.0 --port 8000
