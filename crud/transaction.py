24395cc9c8a0f746f3e46eeb814a1f7c4ccd8ddf
Preqsy <obbyprecious24@gmail.com>
2025-12-13 22:07:46 +0100

    feat: Refactor transaction retrieval methods to reduce code duplication and improve clarity

--- crud/transaction.py
+++ crud/transaction.py
@@ -11,15 +11,11 @@ from schemas.enums import AccountTypeEnum, TransactionTypeEnum
 
 class CRUDTransaction(CRUDBase[Transaction]):
 
-    def get_user_transactions_by_id(self, user_id: int, date: date):
-        month = date.month
-        year = date.year
+    def _get_transaction_query_by_user_id(self, user_id: int):
         return (
             self.db.query(Transaction)
             .filter(
                 Transaction.user_id == user_id,
-                extract("month", Transaction.created_at) == month,
-                extract("year", Transaction.created_at) == year,
                 Transaction.account.has(is_deleted=False),
             )
             .options(
@@ -28,78 +24,44 @@ class CRUDTransaction(CRUDBase[Transaction]):
                 joinedload(Transaction.account),
             )
             .order_by(Transaction.id.desc())
+        )
+
+    def get_user_transactions_by_id(self, user_id: int, date: date):
+        month = date.month
+        year = date.year
+        return (
+            self._get_transaction_query_by_user_id(user_id)
+            .filter(
+                extract("month", Transaction.created_at) == month,
+                extract("year", Transaction.created_at) == year,
+            )
             .all()
         )
 
     def get_transaction_by_id(self, transaction_id: int, user_id: int):
         return (
-            self.db.query(Transaction)
-            .filter(
-                Transaction.id == transaction_id,
-                Transaction.user_id == user_id,
-                Transaction.account.has(is_deleted=False),
-            )
-            .options(
-                joinedload(Transaction.category),
-                joinedload(Transaction.user_currency, innerjoin=True).joinedload(
-                    UserCurrency.currency
-                ),
-                joinedload(Transaction.account),
-            )
+            self._get_transaction_query_by_user_id(user_id)
+            .filter(Transaction.id == transaction_id)
             .first()
         )
 
     def get_automatic_transactions(self, user_id: int):
         return (
-            self.db.query(Transaction)
-            .filter(
-                Transaction.user_id == user_id,
-                Transaction.account.has(account_type=AccountTypeEnum.AUTOMATIC),
-                Transaction.account.has(is_deleted=False),
-            )
-            .options(
-                joinedload(Transaction.category),
-                joinedload(Transaction.user_currency, innerjoin=True).joinedload(
-                    UserCurrency.currency
-                ),
-                joinedload(Transaction.account),
-            )
+            self._get_transaction_query_by_user_id(user_id)
+            .filter(Transaction.account.has(account_type=AccountTypeEnum.AUTOMATIC))
             .all()
         )
 
     def get_transactions_by_account_id(self, account_id: int, user_id: int):
         return (
-            self.db.query(Transaction)
-            .filter(
-                Transaction.account_id == account_id,
-                Transaction.user_id == user_id,
-                Transaction.account.has(is_deleted=False),
-            )
-            .options(
-                joinedload(Transaction.category),
-                joinedload(Transaction.user_currency, innerjoin=True).joinedload(
-                    UserCurrency.currency
-                ),
-                joinedload(Transaction.account),
-            )
+            self._get_transaction_query_by_user_id(user_id)
+            .filter(Transaction.account_id == account_id)
             .all()
         )
 
     def _get_transaction_object_by_category_id(self, category_id: int, user_id: int):
-        return (
-            self.db.query(Transaction)
-            .filter(
-                Transaction.category_id == category_id,
-                Transaction.user_id == user_id,
-                Transaction.account.has(is_deleted=False),
-            )
-            .options(
-                joinedload(Transaction.category),
-                joinedload(Transaction.user_currency, innerjoin=True).joinedload(
-                    UserCurrency.currency
-                ),
-                joinedload(Transaction.account),
-            )
+        return self._get_transaction_query_by_user_id(user_id).filter(
+            Transaction.category_id == category_id,
         )
 
     def get_transaction_by_category_id(self, category_id: int, user_id: int):
@@ -127,23 +89,14 @@ class CRUDTransaction(CRUDBase[Transaction]):
         transaction_date: datetime,
         type: TransactionTypeEnum,
     ):
-        query = (
-            self.db.query(Transaction)
+        return (
+            self._get_transaction_query_by_user_id(user_id)
             .filter(
-                Transaction.user_id == user_id,
                 Transaction.date >= transaction_date,
                 Transaction.transaction_type == type,
-                Transaction.account.has(is_deleted=False),
             )
-            .options(
-                joinedload(Transaction.category),
-                joinedload(Transaction.user_currency, innerjoin=True).joinedload(
-                    UserCurrency.currency
-                ),
-                joinedload(Transaction.account),
-            )
+            .all()
         )
-        return query.all()
 
 
 db_session = next(get_db())
