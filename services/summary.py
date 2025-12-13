eebc68ca2b300b2f2532aadafd0ea871788add1a
Preqsy <obbyprecious24@gmail.com>
2025-12-13 21:46:15 +0100

    feat: Restore account summary logging and summary retrieval logic

--- services/summary.py
+++ services/summary.py
@@ -17,24 +17,24 @@ class AccountSummaryService:
         self.crud_account = crud_account
 
     async def get_account_summary(self, user_id: int, date: date):
-        # print(
-        #     f"Getting summary for user_id: {user_id} and date: {date.month}-{date.year}"
-        # )
-        # summary = self.crud_total_summary.get_total_summary(user_id=user_id, date=date)
-        # if not summary:
-        #     return {
-        #         "user_id": user_id,
-        #         "total_income": 0.0,
-        #         "total_expense": 0.0,
-        #         "net_total": 0.0,
-        #         "total_cash_at_hand": 0.0,
-        #         "total_balance": 0.0,
-        #         "default_user_currency_id": None,
-        #         "default_currency_code": None,
-        #         "month": date.month,
-        #         "year": date.year,
-        #     }
-        summary = await self.calculate_account_balance(user_id=user_id)
+        print(
+            f"Getting summary for user_id: {user_id} and date: {date.month}-{date.year}"
+        )
+        summary = self.crud_total_summary.get_total_summary(user_id=user_id, date=date)
+        if not summary:
+            return {
+                "user_id": user_id,
+                "total_income": 0.0,
+                "total_expense": 0.0,
+                "net_total": 0.0,
+                "total_cash_at_hand": 0.0,
+                "total_balance": 0.0,
+                "default_user_currency_id": None,
+                "default_currency_code": None,
+                "month": date.month,
+                "year": date.year,
+            }
+        # summary = await self.calculate_account_balance(user_id=user_id)
         return summary
 
     async def calculate_account_balance(self, user_id: int):
